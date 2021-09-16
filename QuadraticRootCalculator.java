00be4de14ade3e4174a0aeacd25d684db4bf8edf
dfgrisales5078 <54412831+dfgrisales5078@users.noreply.github.com>
2021-09-15 21:53:03 -0400

    Update QuadraticRootCalculator.java

--- QuadraticRootCalculator.java
+++ QuadraticRootCalculator.java
@@ -1,3 +1,10 @@
+//Diego Grisales
+//Java program that solves a set of quadratic equations 
+//Reads an integer number entered in the command line 
+//And solves as many quadratic equations as the number entered.
+//Using methods and exceptions.
+//11/12/20
+
 import java.util.Scanner; // Import scanner.
 
 public class QuadraticRootCalculator {
